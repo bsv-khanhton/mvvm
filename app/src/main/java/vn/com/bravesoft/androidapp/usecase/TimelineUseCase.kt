c77aa4208217e60daeaecae7b26970f7a5065419
NguyenLinh <nguyenlinh.nttu@gmail.com>
2020-09-15 13:14:17 +0700

    - Prepare 3.1 Timeline

---
+++ app/src/main/java/vn/com/bravesoft/androidapp/usecase/TimelineUseCase.kt
@@ -0,0 +1,12 @@
+package vn.com.bravesoft.androidapp.usecase
+
+import vn.com.bravesoft.androidapp.api.ApiStores
+import javax.inject.Inject
+
+
+/**
+ * Created by Linh Nguyen on 2020-09-15.
+ */
+class TimelineUseCase @Inject constructor(private val apiService: ApiStores) {
+
+}
